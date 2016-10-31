699f680b009bd2f07997d71b88961b3760d34535
Dennis Hedegaard <dennis@dhedegaard.dk>
2016-10-31 22:18:21 +0100

    Some work on the login stuff.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using todo.Models;
 using todo.ViewModels;
@@ -8,10 +11,16 @@ namespace todo
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ModelContext _context;
-
-        public HomeController(ModelContext context)
+        public HomeController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ModelContext context)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
             _context = context;
         }
         public IActionResult Index()
@@ -22,16 +31,16 @@ namespace todo
 
         [HttpPost]
         [ValidateAntiForgeryTokenAttribute]
-        public IActionResult AddNote([FromForm] AddTodo value)
+        public IActionResult AddNote([FromForm] string value)
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid || string.IsNullOrEmpty(value))
             {
                 return BadRequest("Bad request");
             }
 
             var todo = new Todo
             {
-                value = value.value
+                value = value
             };
             _context.Todos.Add(todo);
             _context.SaveChanges();
@@ -51,6 +60,19 @@ namespace todo
             _context.Remove(todo);
             _context.SaveChanges();
             return Redirect("/");
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromForm] string username, [FromForm] string password)
+        {
+            if (Request.Method == "POST" && ModelState.IsValid)
+            {
+                var user = new ApplicationUser{UserName = username};
+                await _signInManager.SignInAsync(user, isPersistent: true);
+                return RedirectToAction(nameof(HomeController.Index), "Index");
+                throw new Exception(username + " - " + password);
+            }
+            return View();
         }
     }
 }
