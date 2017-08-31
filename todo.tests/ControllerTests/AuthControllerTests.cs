0465ae21fc0521d8543f3a1f0b9600ce1f0d8167
Dennis Hedegaard <dennis@dhedegaard.dk>
2017-08-31 19:32:07 +0200

    Initial test project with a few tests.

---
+++ todo.tests/ControllerTests/AuthControllerTests.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using todo.ViewModels;
+using Xunit;
+
+namespace todo.tests
+{
+    public class AuthControllerTest
+    {
+        private readonly AuthController _controller = new AuthController(null, null, null);
+
+        [Fact]
+        public async void Login_Get()
+        {
+            var result = _controller.Login();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsAssignableFrom<LoginUser>(viewResult.Model);
+        }
+
+        [Fact]
+        public async void Register_Get()
+        {
+            var result = _controller.Register();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsAssignableFrom<RegisterUser>(viewResult.Model);
+        }
+    }
+}
